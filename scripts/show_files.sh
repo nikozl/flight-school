56d3d729c7df4c8feb5ae7b8dd1731b9e3ffa705
Nikolay <nikolemur@gmail.com>
2017-02-20 13:11:45 +0100

    changes

--- scripts/show_files.sh
+++ scripts/show_files.sh
@@ -16,12 +16,12 @@ echo " "
 cat generate_file_output/fileip.txt
 
 apt-get update -y
-#apt-get install openssh-client -y
-apt-get insstall openssh-server -y
-apt-get install sshpass -y
-#apt-get install rsync -y
+apt-get install openssh-client -y
+#apt-get insstall openssh-server -y
+#apt-get install sshpass -y
+apt-get install rsync -y
 
 echo " "
-#echo "ejecutando rsync..."
-sshpass -p "123456" scp generate_file_output/fileip.txt root@10.254.0.1:/root/dir2
-#rsync -a generate_file_output / root@10.254.0.1:/root/dir2
+echo "ejecutando rsync..."
+#sshpass -p "123456" scp generate_file_output/fileip.txt root@10.254.0.1:/root/dir2
+rsync -a -o StrictHostKeyChecking=no generate_file_output / root@10.254.0.1:/root/dir2
