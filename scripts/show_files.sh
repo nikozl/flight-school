2f506739b38d15dc7cfdfbbae75527b4bb50ad71
Nikolay <nikolemur@gmail.com>
2017-02-20 14:12:25 +0100

    add

--- scripts/show_files.sh
+++ scripts/show_files.sh
@@ -20,9 +20,20 @@ apt-get install openssh-client -y
 #apt-get insstall openssh-server -y
 apt-get install sshpass -y
 apt-get install rsync -y
+apt-get install git -y
 donde_estoy=$(pwd)
 cat ${donde_estoy}
 echo " "
 echo "ejecutando rsync..."
 #sshpass -p "123456" scp generate_file_output/fileip.txt root@10.254.0.1:/root/dir2
 sshpass -p "123456" rsync -a -e "ssh -o StrictHostKeyChecking=no" ${donde_estoy}/ root@10.254.0.1:/root/dir2
+
+echo " "
+#echo "subir carpeta a git"
+#cd ${donde_estoy}
+#git clone https://github.com/nikozl/tests
+#mv generate_file_output tests
+#git add --all
+#git commit -m "concourse changes"
+#git push origin master
+
