81ee1f12399bb5b651ee070b45203a3725b40b1c
Nikolay <nikolemur@gmail.com>
2017-02-20 12:08:16 +0100

    add files

--- scripts/show_files.sh
+++ scripts/show_files.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 echo "ls general"
-
+echo " "
 ls -l
-
+echo " "
 echo "ls de la carpeta"
-
+echo " "
 ls -lrta generate_file_output
-
+echo " "
 echo "ip de container"
-echo "<br>"
+echo " "
 cat generate_file_output/fileip.txt
 
