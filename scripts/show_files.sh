bf6df976d1e14db7a16bcddcf1f01cf823c1dd2a
Nikolay <nikolemur@gmail.com>
2017-02-20 13:48:14 +0100

    changes

--- scripts/show_files.sh
+++ scripts/show_files.sh
@@ -23,6 +23,6 @@ apt-get install rsync -y
 donde_estoy=$(pwd)
 cat ${donde_estoy}
 echo " "
-#echo "ejecutando rsync..."
+echo "ejecutando rsync..."
 #sshpass -p "123456" scp generate_file_output/fileip.txt root@10.254.0.1:/root/dir2
 sshpass -p "123456" rsync -a -e "ssh -o StrictHostKeyChecking=no" ${donde_estoy}/ root@10.254.0.1:/root/dir2
