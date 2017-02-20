f5ac42d3f4d900f470d3c2ecdee29980858e1ff7
Nikolay <nikolemur@gmail.com>
2017-02-20 12:06:16 +0100

    add

--- scripts/show_files.sh
+++ scripts/show_files.sh
@@ -9,7 +9,6 @@ echo "ls de la carpeta"
 ls -lrta generate_file_output
 
 echo "ip de container"
-
+echo "<br>"
 cat generate_file_output/fileip.txt
 
-sshpass -p "1N1S4K1Z" scp generate_file_output/fileip.txt niko@16.3.252.243/home/niko/fileip.txt
