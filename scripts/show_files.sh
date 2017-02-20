285d246b8345290ca598648d8b105a1c3ee7e9dd
Nikolay <nikolemur@gmail.com>
2017-02-20 12:11:18 +0100

    edit script

--- scripts/show_files.sh
+++ scripts/show_files.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
-
-echo "ls general"
+echo "donde estoy: "
+echo " "
+pwd
+echo " "
+echo "ls general: "
 echo " "
 ls -l
 echo " "
-echo "ls de la carpeta"
+echo "ls de la carpeta: "
 echo " "
 ls -lrta generate_file_output
 echo " "
-echo "ip de container"
+echo "ip de container: "
 echo " "
 cat generate_file_output/fileip.txt
 
