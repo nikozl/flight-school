82f2c9bb5e0da031ae12c5d7edb710dd3c1c1a69
Niko <nikolemur@gmail.com>
2017-11-20 13:29:23 +0100

    add changes

--- mkosi_build.sh
+++ mkosi_build.sh
@@ -10,6 +10,7 @@ cd /root/mkosi
 mkosi --version
 export
 dnf install -y e2fsprogs
-ls -l /dev/
+ls -l /dev/loop*
 mknod /dev/loop1p1 b 7 1
-mkosi -C ${donde}/tests/mkosi_build/repo
+ls -l /dev/loop*
+#mkosi -C ${donde}/tests/mkosi_build/repo
