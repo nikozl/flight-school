c4a35f4e53356649a3e44e3a0a74119594e50fe6
Niko <nikolemur@gmail.com>
2017-11-22 12:56:45 +0100

    add changes to mkosi

--- mkosi_build.sh
+++ mkosi_build.sh
@@ -4,16 +4,11 @@ set -x
 
 ls -lrtha
 donde=$(pwd)
-#cd /usr/bin/
-find / -name mkosi
-#cd /root/mkosi
-#mkosi --version
-#export
 dnf install -y e2fsprogs systemd-container
 mknod /dev/loop1 b 7 1
 chown root:disk /dev/loop*
 chmod 660 /dev/loop*
 ls -l /dev/loop*
-cd /tmp/mkosi
+chmod +x ${donde}/tests/mkosi_build/repo/mkosi.postinst
 #mkosi
-./mkosi -C ${donde}/tests/mkosi_build/repo 2>/tmp/error || cat /tmp/error
+mkosi -C ${donde}/tests/mkosi_build/repo
