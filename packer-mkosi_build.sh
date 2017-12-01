a9c8761740fdb9f65ddd704873c48335524cc465
Niko <nikolemur@gmail.com>
2017-12-01 15:19:58 +0100

    add packer files

--- packer-mkosi_build.sh
+++ packer-mkosi_build.sh
@@ -5,12 +5,12 @@ set -x
 export all_proxy="http://16.0.96.20:3128"
 export http_proxy="http://16.0.96.20:3128"
 export https_proxy="http://16.0.96.20:3128"
-
+export no_proxy="16.0.96.20"
 cd tests/mkosi_packer
 apk add --update qemu qemu-system-x86_64 qemu-img
 
-export all_proxy=""
-export http_proxy=""
-export https_proxy=""
+#export all_proxy=""
+#export http_proxy=""
+#export https_proxy=""
 
 packer build --force mkosi.json
