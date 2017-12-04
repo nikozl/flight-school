77a9435458831731a34354709c2cc49aed29f743
Niko <nikolemur@gmail.com>
2017-12-04 14:48:44 +0100

    add packer files

--- mkosi_packer/scripts/mkosi.sh
+++ mkosi_packer/scripts/mkosi.sh
@@ -4,4 +4,4 @@ set -x
 
 cd /tmp/mkosi
 sed -i "s/DEBIAN_FRONTEND/http_proxy': 'http:\/\/16.0.96.20:3128', 'DEBIAN_FRONTEND/g" mkosi
-./mkosi -C /tmp/files
+./mkosi -C /tmp/files --output-dir=/tmp/output/
