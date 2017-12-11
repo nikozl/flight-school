7c4d8d66f7e869f0a84ea06dc7768df6bdd9b0b9
Niko <nikolemur@gmail.com>
2017-12-11 12:36:07 +0100

    changing packer files

--- mkosi_packer/scripts/mkosi.sh
+++ mkosi_packer/scripts/mkosi.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
+cd /tmp/mkosi
 sed -i "s/DEBIAN_FRONTEND/http_proxy': 'http:\/\/16.0.96.20:3128', 'DEBIAN_FRONTEND/g" mkosi
 mkdir -p /tmp/output
-cd /tmp/mkosi
 ./mkosi -C /tmp/files --output-dir=/tmp/output/
