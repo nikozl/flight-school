3feb7cc3f307719d6be1e56fc2299c0a5a417567
Niko <nikolemur@gmail.com>
2017-12-12 14:48:13 +0100

    changing packer files

--- scripts/show_files.sh
+++ scripts/show_files.sh
@@ -12,14 +12,14 @@ apt-get update -y
 apt-get install openssh-client -y
 apt-get install openssh-server -y
 apt-get install sshpass -y
-#apt-get install rsync -y
+apt-get install rsync -y
 #apt-get install git -y
 #donde_estoy=$(pwd)
 #cat ${donde_estoy}
 #echo " "
 #echo "ejecutando rsync..."
-sshpass -p "123456" scp generate_file_output/fileip.txt root@10.254.0.1:/root/dir2
-#sshpass -p "123456" rsync -a -e "ssh -o StrictHostKeyChecking=no" generate_file_output/ root@10.254.0.9:/root/dir2
+#sshpass -p "123456" scp generate_file_output/fileip.txt root@10.254.0.1:/root/dir2
+sshpass -p "123456" rsync -a -e "ssh -o StrictHostKeyChecking=no" generate_file_output/ root@10.254.0.9:/root/dir2
 
 #echo " "
 
